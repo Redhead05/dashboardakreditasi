b4f860e68afc70fdb2afb05d826333ebdd61fd87
vincent <daylightzone91@gmail.com>
2023-12-28 17:45:20 +0700

    update chart

--- resources/js/components/DataTahunBerjalan.vue
+++ resources/js/components/DataTahunBerjalan.vue
@@ -1,59 +1,152 @@
 <template>
-  <div class="m-4">
-    <div class="d-flex justify-content-between">
-      <div class="title">Data Tahun Berjalan</div>
-      <div class="d-flex flex-row justify-content-end gap-2 w-25">
-        <select class="form-select">
-          <option selected>Semua</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-      </div>
+    <div class="m-4">
+        <div class="d-flex justify-content-between">
+            <div class="title">Data Tahun Berjalan</div>
+            <div class="d-flex flex-row justify-content-end gap-2 w-25">
+                <select class="form-select">
+                    <option selected>Semua</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-sm-4">
+                <div class="card position-relative">
+                    <div class="dataLabels_chart d-flex flex-column align-items-center justify-content-center">
+                        <div class="dataLabels_Total">{{total[0]}}</div>
+                        <div class="dataLabels_Lembaga">Lembaga</div>
+                    </div>
+                    <apexchart :width="width" type="radialBar" :options="chartOptionsRadialBar1" :series="seriesRadialBar1"></apexchart>
+                    <div class="chart_title">Sasaran Akreditasi</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card position-relative">
+                    <div class="dataLabels_chart d-flex flex-column align-items-center justify-content-center">
+                        <div class="dataLabels_Total">{{total[1]}}</div>
+                        <div class="dataLabels_Lembaga">Lembaga</div>
+                    </div>
+                    <apexchart :width="width" type="radialBar" :options="chartOptionsRadialBar2" :series="seriesRadialBar2"></apexchart>
+                    <div class="chart_title">Kadaluarsa</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card position-relative">
+                    <div class="dataLabels_chart d-flex flex-column align-items-center justify-content-center">
+                        <div class="dataLabels_Total">{{total[2]}}</div>
+                        <div class="dataLabels_Lembaga">Lembaga</div>
+                    </div>
+                    <apexchart :width="width" type="radialBar" :options="chartOptionsRadialBar3" :series="seriesRadialBar3"></apexchart>
+                    <div class="chart_title">Potensi Reakreditasi</div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="row mt-4">
-      <div class="col-sm-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-
+const width = 420;
+const hollowSize = '50%';
+const total = ['40.094','12.094','10.091'];
+const chartOptionsRadialBar1 = {
+    colors: ['var(--color_dikbud_3)'],
+    plotOptions: {
+        radialBar: {
+            hollow: {
+                size: hollowSize,
+            },
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: false,
+                },
+            },
+        },
+    },
+}
+const seriesRadialBar1 = [100]
+const chartOptionsRadialBar2 = {
+    colors: ['var(--color_dikbud_5)'],
+    plotOptions: {
+        radialBar: {
+            hollow: {
+                size: hollowSize,
+            },
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: false,
+                },
+            },
+        },
+    },
+}
+const seriesRadialBar2 = [100]
+const chartOptionsRadialBar3 = {
+    colors: ['var(--color_dikbud_2)'],
+    plotOptions: {
+        radialBar: {
+            hollow: {
+                size: hollowSize,
+            },
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: false,
+                },
+            },
+        },
+    },
+}
+const seriesRadialBar3 = [100]
 </script>
 
 <style scoped>
-  .title{
+.card{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.title{
     color: var(--color_dikbud_4);
     font-size: 18px !important;
-  }
-  .form-select {
+}
+.form-select {
     color: var(--color_dikbud_4);
     width: 50%;
-  }
+}
+.dataLabels_Total{
+    color: var(--color_dikbud_4);
+    font-size: 24px !important;
+}
+.dataLabels_Lembaga{
+    color: var(--color_dikbud_4);
+    font-size: 12px !important;
+}
+.chart_title {
+    color: var(--color_dikbud_4);
+    text-align: center;
+    margin-bottom: 5%;
+}
+.dataLabels_chart{
+    position:absolute;
+    z-index: 10;
+    top: calc(23% + 50px);
+    left: calc(7% + 150px);
+}
+
+@media (min-width: 1400px) {
+    .dataLabels_chart{
+        left: calc(19% + 150px) !important;
+    }
+}
 </style>
--- resources/js/components/DataTahunTerakhir.vue
+++ resources/js/components/DataTahunTerakhir.vue
@@ -1,224 +1,256 @@
 <template>
-  <div class="m-4">
-    <div class="d-flex justify-content-between">
-      <div class="title">Data Tahun Terakhir</div>
-      <div class="d-flex flex-row justify-content-end gap-2 w-25">
-        <select class="form-select">
-          <option selected>Semua</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <select @change="selectYear" class="form-select">
-          <option>2018</option>
-          <option>2019</option>
-          <option>2020</option>
-          <option>2021</option>
-        </select>
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col-sm-7">
-        <div class="card">
-          <div class="card-body">
-            <div class="row ">
-              <div class="col separator_col px-4">
-                <h5 class="card-title mb-1">Capaian Nasional</h5>
-                <h5 class="card3_desc mb-4">{{ datas }}</h5>
-                <apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>
-              </div>
-              <div class="col mx-4">
-                <div class="d-flex flex-column">
-                  <div class="d-flex flex-row align-items-center my-4">
-                    <Icon class="card1_circle_icon" icon="bxs:school" color="#FFFFFF" width="90" height="90"/>
-                    <div class="mx-4 d-flex flex-column">
-                      <div class="card3_num">54.028</div>
-                      <div class="card3_desc">Total Populasi</div>
-                    </div>
-                  </div>
-                  <div class="d-flex flex-row align-items-center my-4">
-                    <apexchart width="210" type="radialBar" :options="chartOptionsRadialBar1" :series="seriesRadialBar1"></apexchart>
-                    <div class="mx-4 d-flex flex-column">
-                      <div class="card3_num">44.094</div>
-                      <div class="card3_desc">Di Akreditasi</div>
-                    </div>
-                  </div>
-                  <div class="d-flex flex-row align-items-center my-4">
-                    <apexchart width="210" type="radialBar" :options="chartOptionsRadialBar2" :series="seriesRadialBar2"></apexchart>
-                    <div class="mx-4 d-flex flex-column">
-                      <div class="card3_num">10.283</div>
-                      <div class="card3_desc">Belum di Akreditasi</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div class="m-4">
+        <div class="d-flex justify-content-between">
+            <div class="title">Data Tahun Terakhir</div>
+            <div class="d-flex flex-row justify-content-end gap-2 w-25">
+                <select class="form-select">
+                    <option selected>Semua</option>
+                    <option>Kota Surabaya</option>
+                    <option>Kota Kediri</option>
+                    <option>Kabupaten Lamongan</option>
+                    <option>dst</option>
+                </select>
+                <select @change="selectYear" class="form-select">
+                    <option>2018</option>
+                    <option>2019</option>
+                    <option>2020</option>
+                    <option>2021</option>
+                </select>
             </div>
-          </div>
         </div>
-      </div>
-      <div class="col-sm-5">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title mb-1">Capaian Sasaran</h5>
-            <h5 class="card3_desc mb-4">Tahun 2022</h5>
-            <apexchart type="donut" width="500" :options="chartOptionsDonut" :series="seriesDonut"></apexchart>
-          </div>
+        <div class="row gx-2 mt-4">
+            <div class="col-sm-7">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row gx-5">
+                            <div class="col-8 separator_col">
+                                <h5 class="card-title mb-1">Capaian Nasional</h5>
+                                <h5 class="card3_desc mb-4">Total 44.094</h5>
+                                <apexchart :width="widthChart" type="bar" :options="chartOptions" :series="series"></apexchart>
+                            </div>
+                            <div class="col-4">
+                                <div class="d-flex flex-column h-90">
+                                    <div class="d-flex flex-row align-items-center margin_data_jumlah_asesi mb-3">
+                                        <Icon class="card1_circle_icon" icon="bxs:school" color="#FFFFFF" width="100" height="100"/>
+                                        <div class="mx-4 d-flex flex-column">
+                                            <div class="card3_num">54.028</div>
+                                            <div class="card3_desc">Total Populasi</div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center margin_data_jumlah_asesi">
+                                        <apexchart :width="widthRadialBar" type="radialBar" :options="chartOptionsRadialBar1" :series="seriesRadialBar1"></apexchart>
+                                        <div class="d-flex flex-column desc_data_jumlah_asesi">
+                                            <div class="card3_num">44.094</div>
+                                            <div class="card3_desc">Di Akreditasi</div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center margin_data_jumlah_asesi">
+                                        <apexchart :width="widthRadialBar" type="radialBar" :options="chartOptionsRadialBar2" :series="seriesRadialBar2"></apexchart>
+                                        <div class="d-flex flex-column desc_data_jumlah_asesi">
+                                            <div class="card3_num">10.283</div>
+                                            <div class="card3_desc">Belum di Akreditasi</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <div class="card card_height">
+                    <div class="card-body">
+                        <h5 class="card-title mb-1">Capaian Sasaran</h5>
+                        <h5 class="card3_desc mb-4">Tahun 2022</h5>
+                        <div class="d-flex flex-row align-items-center justify-content-center ">
+                            <apexchart type="donut" width="400" :options="chartOptionsDonut" :series="seriesDonut"></apexchart>
+                            <div class="d-flex flex-column">
+                                <div class="d-flex flex-row justify-content-center align-items-center">
+                                    <div class="circle_icon" :style="{ 'background-color': capaianSasaran[0].Color }"></div>
+                                    <div class="ms-2 d-flex flex-column">
+                                        <div class="card3_num">{{ capaianSasaran[0].Total }}</div>
+                                        <div class="card3_desc">{{ capaianSasaran[0].Desc }}</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row justify-content-center align-items-center">
+                                    <div class="circle_icon" :style="{ 'background-color': capaianSasaran[1].Color }"></div>
+                                    <div class="ms-2 d-flex flex-column">
+                                        <div class="card3_num">{{ capaianSasaran[1].Total }}</div>
+                                        <div class="card3_desc">{{ capaianSasaran[1].Desc }}</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-row justify-content-center align-items-center">
+                                    <div class="circle_icon" :style="{ 'background-color': capaianSasaran[2].Color }"></div>
+                                    <div class="ms-2 d-flex flex-column">
+                                        <div class="card3_num">{{ capaianSasaran[2].Total }}</div>
+                                        <div class="card3_desc">{{ capaianSasaran[2].Desc }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
-
 <script lang="ts" setup>
+import {router} from "@inertiajs/vue3";
+import {Icon} from "@iconify/vue";
 
-import { onMounted } from 'vue';
-import { router } from '@inertiajs/vue3';
-
-
-  const props = defineProps({
-            datas : Array,
-        })
-  //solusi lain selain props bisa pakai stateManagement
-
-  onMounted(() => {
-    console.log(props.datas);
-    // console.log(props.sumKuotas);
-  })
-
-  const chartOptions = {
+const widthChart = '100%';
+const chartOptions = {
     xaxis: {
-      categories: ['A', 'B', 'C', 'TT',]
+        categories: ['PAUD', 'PKDM', 'SD', 'SMP', 'SMA', ]
     },
-    colors: ['var(--color_dikbud_3)'],
-    // plotOptions: {
-    //   bar: {
-    //     dataLabels: {
-    //       enabled: false, // Set this to false to hide values inside bars
-    //     },
-    //   },
-    // },
-  }
-  const series = [{
-    name: 'series-1',
-    data: [60000, 80000, 60000, 20000,]
-  }]
-  const chartOptionsRadialBar1 = {
-    colors: ['var(--color_dikbud_3)'],
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          size: '30%',
-        },
-        // offsetX: -70,
-        // offsetY: -40,
-        dataLabels: {
-          name: {
-            show: false,
-          },
-          value: {
-            show: true,
-          },
-        },
-      },
-    },
-  }
-  const seriesRadialBar1 = [80.2]
-  const chartOptionsRadialBar2 = {
+    // colors: ['var(--color_dikbud_3)'],
+}
+const series = [{
+    name: 'A',
+    data: [44, 55, 57, 56, 61]
+}, {
+    name: 'B',
+    data: [76, 85, 101, 98, 87]
+}, {
+    name: 'C',
+    data: [35, 41, 36, 26, 45]
+}, {
+    name: 'TT',
+    data: [35, 41, 36, 26, 45]
+}];
+const widthRadialBar = '75%';
+const chartOptionsRadialBar1 = {
     colors: ['var(--color_dikbud_3)'],
     plotOptions: {
-      radialBar: {
-        hollow: {
-          size: '30%',
+        radialBar: {
+            hollow: {
+                size: '30%',
+            },
+            offsetX: -55,
+            offsetY: -20,
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: true,
+                },
+            },
         },
-        // offsetX: -70,
-        // offsetY: -40,
-        dataLabels: {
-          name: {
-            show: false,
-          },
-          value: {
-            show: true,
-          },
-        },
-      },
     },
-  }
-  const seriesRadialBar2 = [20.8]
-  const chartOptionsDonut = {
+}
+const seriesRadialBar1 = [80.2]
+const chartOptionsRadialBar2 = {
+    colors: ['var(--color_dikbud_3)'],
+    plotOptions: {
+        radialBar: {
+            hollow: {
+                size: '30%',
+            },
+            offsetX: -55,
+            offsetY: -20,
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: true,
+                },
+            },
+        },
+    },
+}
+const seriesRadialBar2 = [20.8]
+const capaianSasaran = [
+    {
+        Color: 'var(--color_primary_normal)',
+        Total: '14.876',
+        Desc: 'Total Sasaran',
+    },
+    {
+        Color: 'var(--color_dikbud_2)',
+        Total: '10.837',
+        Desc: 'Akreditasi Baru',
+    },
+    {
+        Color: 'var(--color_neutral_90)',
+        Total: '4.983',
+        Desc: 'Reakreditasi',
+    },
+]
+const colorsDonut = capaianSasaran.map(a => a.Color)
+const chartOptionsDonut = {
     dataLabels: {
-      enabled: false
+        enabled: false
     },
     legend: {
-      position: 'right',
-      // formatter: function(val:any, opts:any) {
-      //   return `
-      //     <div style="
-      //       color: #213240;
-      //       font-family: Inter;
-      //       font-size: 24px;
-      //       font-style: normal;
-      //       font-weight: 600;
-      //       line-height: normal;
-      //     ">${opts.w.globals.series[opts.seriesIndex].total}</div>
-      //     <div style="
-      //       color: #213240;
-      //       font-family: Inter;
-      //       font-size: 12px;
-      //       font-style: normal;
-      //       font-weight: 300;
-      //       line-height: normal;
-      //       margin-bottom: 15%;
-      //     ">${opts.w.globals.series[opts.seriesIndex].desc}</div>
-      //   `
-      // }
-    }
-  }
-  const seriesDonut = [
-    // {value:80,total:'14.876',desc:'Total Sasaran'},
-    // {value:15,total:'10.837',desc:'Akreditasi Baru'},
-    // {value:5,total:'4.983',desc:'Reakreditasi'},
+        show:false,
+    },
+    colors: colorsDonut
+}
+const seriesDonut = [
     80,15,5
-  ];
+];
 
-  const selectYear = (event:any) => {
+const selectYear = (event:any) => {
     const url = window.location.origin;
     // console.log(event.target.value, url);
     router.get(url, {
-      year: event.target.value //query param
+        year: event.target.value //query param
     }, {preserveState: true});
-  }
+}
 </script>
 
 <style scoped>
-  .title{
+.title{
     color: var(--color_dikbud_4);
     font-size: 18px !important;
-  }
-  .form-select {
+}
+.form-select {
     color: var(--color_dikbud_4);
     width: 50%;
-  }
-  .card1_circle_icon{
+}
+.card1_circle_icon{
     padding: 1.2em;
     background-color: var(--color_dikbud_3);
     border-radius: 50%;
-  }
-
-  .card3_num{
+}
+.card_height {
+    height: 100% !important;
+}
+.card3_num{
     color: var(--color_dikbud_4);
     font-size: 24px !important;
-  }
-  .card3_desc{
+}
+.card3_desc{
     color: var(--color_dikbud_4);
     font-size: 12px !important;
     font-weight: 300 !important;
-  }
-  .separator_col{
-    height: 100%;
+}
+.separator_col{
     border-right: 1px solid var(--color_neutral_90);
-    padding: 0;
-    margin: 0;
-  }
-</style>
+}
+.circle_icon {
+    width: 16px;
+    height: 16px;
+    border-radius: 100%;
+}
+.margin_data_jumlah_asesi {
+    margin: 3% 0;
+    position: relative;
+}
+.desc_data_jumlah_asesi {
+    position:absolute;
+    z-index: 10;
+    top: 3vh;
+    left: 8.5vw;
+}
 
+@media (min-width: 1400px) {
+    .desc_data_jumlah_asesi{
+        top: 2.5vh;
+        left: 7vw;
+    }
+}
+</style>
